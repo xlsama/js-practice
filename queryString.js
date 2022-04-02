b0250f0ccbc16ff9aee3ad4c0d1b5c14412235be
xlsama <im.xlsama@gmail.com>
2022-04-02 16:23:18 +0800

    chore:update

--- queryString.js
+++ queryString.js
@@ -1,21 +1,7 @@
-// output  {a:2,b:2}
-// https://bytedance.feishu.cn/docx/doxcnwtQMB6t5UY?a=2&b=2;
-
-// const getParams = url => {
-//   const str = url.slice(url.indexOf('?') + 1)
-//   const params = {}
-//   str.split('&').forEach(item => {
-//     const arr = item.split('=')
-//     params[arr[0]] = arr[1]
-//   })
-
-//   return params
-// }
-
 const queryString = str => {
   const obj = {}
-  str.replace(/([^?&=]+)=([^&]+)/g, (_, k, v) => (obj[k] = v))
+  new URL(str).searchParams.forEach((v, k) => (obj[k] = v))
   return obj
 }
 
-console.log(queryString('https://bytedance.feishu.cn/docx/doxcnwtQMB6t5UY?a=2&b=2'))
+console.log(queryString('https://baidu.com?a=1&b=2'))
