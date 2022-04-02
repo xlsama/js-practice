a9590d8dcc334410fa9e1f35d4be145caf8d77fa
xlsama <im.xlsama@gmail.com>
2022-04-02 17:26:25 +0800

    update

--- trim.js
+++ trim.js
@@ -2,4 +2,23 @@ function trim(str) {
   return str.replace(/^(\s)+|(\s)+$/g, '')
 }
 
-console.log(trim('   a  b c ')) // 'a  b c'
+// function trim(str) {
+//   let start
+//   let end
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] !== ' ') {
+//       start = i
+//       break
+//     }
+//   }
+//   for (let i = str.length; i > 0; i--) {
+//     if (str[i] !== ' ') {
+//       end = i
+//       break
+//     }
+//   }
+
+//   return str.substring(start, end)
+// }
+
+console.log(trim('   a  b c '))
